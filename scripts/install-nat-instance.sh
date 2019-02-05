0168f0ccbb156d89080aa7b325c6e2652d08cd25
Adriaan Peeters <Adriaan_Peeters@peopleware.be>
2019-02-05 18:36:52 +0100

    Determine interface name, required to support ENA instances

--- scripts/install-nat-instance.sh
+++ scripts/install-nat-instance.sh
@@ -6,12 +6,14 @@ set -e -x
 # Configuration
 VPC_CIDR="${vpc_cidr}"
 
+INTERFACE=`ip route list | grep default | grep -E 'dev (\w+)' -o | awk '{print $2}'`
+
 # Enable ipv4 ip forwarding
 echo net.ipv4.ip_forward=1 > /etc/sysctl.d/60-ip_forward.conf
 /usr/sbin/service procps restart
 
 # Enable NAT
 mkdir -p /etc/network/if-up.d/
-echo "iptables -t nat -A POSTROUTING -o eth0 -s $VPC_CIDR -j MASQUERADE" > /etc/network/if-up.d/forwarding
+echo "iptables -t nat -A POSTROUTING -o $INTERFACE -s $VPC_CIDR -j MASQUERADE" > /etc/network/if-up.d/forwarding
 chmod 755 /etc/network/if-up.d/forwarding
 /etc/network/if-up.d/forwarding
